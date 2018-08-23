a445d083fc2e95e72324cf2216e066d639b0e7fd
Matt Hall <matthall8@gmail.com>
2018-08-23 19:47:10 +0300

    Replaced dummy query with DB query #25

--- src/controllers/eachGraduate.js
+++ src/controllers/eachGraduate.js
@@ -1,24 +1,21 @@
-const profiles = require('../model/index.js');
 const { getUserData } = require('../model/getUserData');
 
 exports.get = (req, res, next) => {
-  getUserData((err, result) => {
+  getUserData((err, profiles) => {
     if (err) {
-      console.log('This error is', err);
       return res.render('error');
     }
-    console.log(result);
     const { eachGraduate } = req.params;
     let exist = false;
     let tmp = 0;
-    result.forEach((user, index) => {
+    profiles.forEach((user, index) => {
       if (user.git_username == eachGraduate) {
         exist = true;
         tmp = index;
       }
     });
     if (exist) {
-      return res.render('profiles', result[tmp]);
+      return res.render('profiles', profiles[tmp]);
     }
     next();
   });
--- src/model/getUserData.js
+++ src/model/getUserData.js
@@ -2,13 +2,10 @@ const dbConnection = require('../../database/dbconnection');
 
 // Gets everything from the users table and returns it in a callback
 const getUserData = (cb) => {
-  console.log('Made it');
   dbConnection.query('SELECT * FROM users', (err, res) => {
     if (err) {
-      console.log(err);
       cb(err);
     } else {
-      console.log(res.rows);
       cb(null, res.rows);
     }
   });
