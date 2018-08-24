ee6e36f7ff0ed20f22f6a827cee5b8a6181c0fd3
Matt Hall <matthall8@gmail.com>
2018-08-24 13:36:47 +0300

    Edit CV now works for each user

--- src/controllers/edit.js
+++ src/controllers/edit.js
@@ -1,17 +1,21 @@
 const { updateCV } = require('../model/updateUserData');
 
 exports.get = (req, res) => {
-  res.render('editinfo', {
-    title: 'Edit User Profile',
-    logged_in: req.session.logged_in,
-    git_username: req.session.git_username,
-  });
+  if (req.session.logged_in) {
+    res.render('editinfo', {
+      title: 'Edit User Profile',
+      logged_in: req.session.logged_in,
+      git_username: req.session.git_username,
+    });
+  } else {
+    res.redirect('/');
+  }
 };
 
 exports.post = (req, res) => {
-  const userId = 5;
+  const username = req.session.git_username;
   // const userId = req.session.userid;
-  updateCV(userId, req.body.cv, (err) => {
+  updateCV(username, req.body.cv, (err) => {
     if (err) {
       console.log(err);
     }
--- src/model/updateUserData.js
+++ src/model/updateUserData.js
@@ -1,10 +1,10 @@
 const dbConnection = require('../database/dbconnection');
 
 // Updates the user's CV, called in controllers/edit.js
-const updateCV = (userId, cv, cb) => {
+const updateCV = (gitUsername, cv, cb) => {
   const queryString = `UPDATE users SET cv = $1
-                      WHERE id = $2`;
-  dbConnection.query(queryString, [cv, userId], (err) => {
+                      WHERE git_username = $2`;
+  dbConnection.query(queryString, [cv, gitUsername], (err) => {
     if (err) {
       cb(err);
     } else {
